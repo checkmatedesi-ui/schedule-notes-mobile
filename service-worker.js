0ea4d327093a32fcba2ff6b804ad3d9f6d3b4599
checkmatedesi-ui <checkmatedesi@gmail.com>
2026-09-04 15:11:36 +0800

    Fix silent course generation validation and support academic-year ranges

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'schedule-shell-2026.09.04.2';
+const CACHE = 'schedule-shell-2026.09.04.3';
 const asset = (path = '') => new URL(path, self.registration.scope).href;
 const SHELL = [
   asset(''),
