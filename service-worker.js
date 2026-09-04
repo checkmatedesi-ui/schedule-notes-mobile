4e849419babe19fbd07a029e73c63c026afebba5
checkmatedesi-ui <checkmatedesi@gmail.com>
2026-09-04 15:32:44 +0800

    Polish course details and add batch management

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'schedule-shell-2026.09.04.4';
+const CACHE = 'schedule-shell-2026.09.04.5';
 const asset = (path = '') => new URL(path, self.registration.scope).href;
 const SHELL = [
   asset(''),
