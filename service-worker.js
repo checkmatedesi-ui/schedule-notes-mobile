abdaf5319a59a43bffb8632094d368e9ef887f84
checkmatedesi-ui <checkmatedesi@gmail.com>
2026-09-07 15:33:44 +0800

    Fix ultimate easter egg on mobile

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "schedule-shell-2026.09.07.2";
+const CACHE = "schedule-shell-2026.09.07.3";
 const asset = (path = "") => new URL(path, self.registration.scope).href;
 const SHELL = [
   asset(""),
